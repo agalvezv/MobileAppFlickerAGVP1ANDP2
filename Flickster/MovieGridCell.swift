8e2ced8e13c1fcec7af6a63bb671396dd94bf2a9
agalvezv <43121059+agalvezv@users.noreply.github.com>
2022-09-13 05:13:55 -0400

    Updating Project With Actual Proj
    Uploading

---
+++ Flickster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flickster
+//
+//  Created by Alexandro Galvez-Vega on 9/13/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
---
+++ Flickster/TableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewCell.swift
+//  Flickster
+//
+//  Created by Alexandro Galvez-Vega on 9/6/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
